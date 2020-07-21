b3877df5491bb7f89c9833906a5e0cbeeac48001
Ling Cheng Nan <nan800130@gmail.com>
2020-07-21 16:38:48 +0800

    sss

---
+++ authorization.js
@@ -0,0 +1,3 @@
+function RedirectHome() {
+    window.location.href = 'http://140.120.14.210/xamppTestLinePay/index.html';
+}
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // import packages
-const uuid = require('uuid4');
-const crypto = require('crypto-js');
-const axios = require('axios');
+const uuid    = require('uuid4');
+const crypto  = require('crypto-js');
+const axios   = require('axios');
 const LinePay = require('line-pay-v3');
 
 // setting parameters
@@ -20,7 +20,6 @@ const linePay = new LinePay({
 })
 
 //prepare an arbitary order
-
 const order = {
     "amount": 100,
     "currency": "TWD",
@@ -33,7 +32,7 @@ const order = {
                 {
                     "id": "PEN-B-001",
                     "name": "TEST PRODUCT",
-                    "imageUrl": "https://img.shopping.friday.tw/images/product/235/7061302/7061302_3_1.jpg?671963",
+                    "imageUrl": "https://cdn.kingstone.com.tw/cvlife/images/product/20880/2088040323257/2088040323257b.jpg",
                     "quantity": 2,
                     "price": 50
                 }
@@ -41,18 +40,18 @@ const order = {
         }
     ],
     "redirectUrls": {
-        "confirmUrl": "http://140.120.14.210/nodetest/payment/authorize.html",
-        "cancelUrl": "http://140.120.14.210/nodetest/payment/cancel.html"
+        "confirmUrl": "http://140.120.14.210/xamppTestLinePay/payment/authorize.html",
+        "cancelUrl": "http://140.120.14.210/xamppTestLinePay/payment/cancel.html"
     }
 }
 
 // linePay.request(order).then(res => {console.log(res)});
 
 //encrypted HMAC-Base64 Signature formula
-let encrypt = crypto.HmacSHA256(YOUR_CHANNELSECRET + requestUri + JSON.stringify(order) + nonce, YOUR_CHANNELSECRET)
+let encrypt = crypto.HmacSHA256(YOUR_CHANNELSECRET + requestUri + JSON.stringify(order) + nonce, YOUR_CHANNELSECRET);
 
 //transform into base64
-let hmacBase64 = crypto.enc.Base64.stringify(encrypt)
+let hmacBase64 = crypto.enc.Base64.stringify(encrypt);
 
 //HTTP request header configuration
 let configs = {
@@ -62,21 +61,26 @@ let configs = {
         'X-LINE-Authorization-Nonce': nonce,
         'X-LINE-Authorization': hmacBase64
     }
-}
+};
 
 
-console.log("Channel Id: " + YOUR_CHANNEL_ID)
-console.log("Channel Secret Key" + YOUR_CHANNELSECRET)
-console.log("URI : " + uri)
-console.log("Request URI: " + requestUri)
-console.log("Nonce : " + nonce)
-console.log("HMAC Base64 : " + hmacBase64)
+console.log("Channel Id: " + YOUR_CHANNEL_ID);
+console.log("Channel Secret Key" + YOUR_CHANNELSECRET);
+console.log("URI : " + uri);
+console.log("Request URI: " + requestUri);
+console.log("Nonce : " + nonce);
+console.log("HMAC Base64 : " + hmacBase64);
 var reservedUrl = '';
 
 axios.post('https://sandbox-api-pay.line.me/v3/payments/request', order, configs).then(res => {
     // lineUrl = res.data.info.paymentUrl.web;
     console.log(res.data);
-    reservedUrl = res.data.info.paymentUrl.web;
-    console.log('web url => ', reservedUrl);
+    paymentRedirctUrl = res.data.info.paymentUrl.web;
+    console.log('paymentRedirctUrl => ', paymentRedirctUrl);
 });
 
+console.log('web2 url => ', reservedUrl);
+
+function payConfirm() {
+    window.location.href = 'https://sandbox-web-pay.line.me/web/payment/waitPreLogin?transactionReserveId=YWFLZHcwZ1Q3Unc0SWVTeDBOcWFTUUR6czA4VEFwOFhXcXRWYmt1NjZpdzhtdzlCUkF0clJWdXB2Vnd2Si93Yw';
+}
